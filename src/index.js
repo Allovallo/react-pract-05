32024070c167c040bb283eb974c81e141e8af756
Allovallo <grossco@meta.ua>
2024-09-19 17:51:19 +0300

    files updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
+class Toggle extends Component {
+  state = { isOpen: false };
+
+  show = () => this.setState({ isOpen: true });
+
+  hide = () => this.setState({ isOpen: false });
+
+  render() {
+    const { isOpen } = this.state;
+    const { children } = this.props;
+
+    return (
+      <>
+        <button onClick={this.show}>Show</button>
+        <button onClick={this.hide}>Hide</button>
+        {isOpen && children}
+      </>
+    );
+  }
+}
+
 class Counter extends Component {
   static defaultProps = {
     step: 1,
+    initialValue: 0,
+  };
+
+  state = {
+    value: this.props.initialValue,
+  };
+
+  handleIncrement = (e) => {
+    console.log("IB was clicked!", e);
+    console.log("this.props: ", this.props);
+  };
+
+  handleDecrement = (e) => {
+    console.log("DB was clicked!", e);
+    console.log("this.props: ", this.props);
   };
 
   render() {
@@ -12,22 +48,10 @@ class Counter extends Component {
     return (
       <div>
         <span>0</span>
-        <button
-          type="button"
-          onClick={(e) => {
-            console.log("IB was clicked!", e);
-            console.log("this.props: ", this.props);
-          }}
-        >
+        <button type="button" onClick={this.handleIncrement}>
           Increment by {step}
         </button>
-        <button
-          type="button"
-          onClick={(e) => {
-            console.log("DB was clicked!", e);
-            console.log("this.props: ", this.props);
-          }}
-        >
+        <button type="button" onClick={this.handleDecrement}>
           Decrement by {step}
         </button>
       </div>
