0d04f3b10838b1c69ca9572d7ee89759eff956fc
Allovallo <grossco@meta.ua>
2024-09-20 18:17:47 +0300

    Form updated

--- src/index.js
+++ src/index.js
@@ -1,97 +1,42 @@
-// import React, { Component } from "react";
-// import ReactDOM from "react-dom/client";
-
-// class Toggle extends Component {
-//   state = { isOpen: false };
-
-//   toggle = () => {
-//     this.setState((state) => ({ isOpen: !state.isOpen }));
-//   };
-
-//   // show = () => this.setState({ isOpen: true });
-
-//   // hide = () => this.setState({ isOpen: false });
-
-//   render() {
-//     const { isOpen } = this.state;
-//     const { children } = this.props;
-
-//     return (
-//       <>
-//         <button onClick={this.toggle}>{isOpen ? "Hide" : "Show"}</button>
-//         {/* <button onClick={this.show}>Show</button>
-//         <button onClick={this.hide}>Hide</button> */}
-//         {isOpen && children}
-//       </>
-//     );
-//   }
-// }
-
-// class Counter extends Component {
-//   static defaultProps = {
-//     step: 1,
-//     initialValue: 0,
-//   };
-
-//   state = {
-//     value: this.props.initialValue,
-//   };
-
-//   handleIncrement = () => {
-//     this.setState((state, props) => ({ value: state.value + props.step }));
-//   };
-
-//   handleDecrement = () => {
-//     this.setState((state, props) => ({ value: state.value - props.step }));
-//   };
-
-//   render() {
-//     const { step } = this.props;
-
-//     return (
-//       <div>
-//         <span>0</span>
-//         <button type="button" onClick={this.handleIncrement}>
-//           Increment by {step}
-//         </button>
-//         <button type="button" onClick={this.handleDecrement}>
-//           Decrement by {step}
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <Counter step={5} />
-// );
-
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-class LoginForm extends Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
+class SignUpForm extends Component {
+  state = {
+    login: "",
+  };
 
-    const login = form.elements.login.value;
-    const password = form.elements.password.value;
+  handleChange = (e) => {
+    this.setState({ login: e.target.value });
+  };
 
-    console.log(login, password);
-
-    this.props.onSubmit({ login, password });
-    form.reset();
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(`Signed up as: ${this.state.login}`);
+    this.props.onSibmit({ ...this.state });
   };
 
   render() {
+    const { login } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" name="login" />
-        <input type="password" name="password" />
-        <button type="submit">Log in</button>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Enter login"
+            value={login}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <button type="submit">Sign up as {login}</button>
       </form>
     );
   }
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<LoginForm />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SignUpForm onSibmit={(values) => console.log(values)} />
+);
